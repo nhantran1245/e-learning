a1ebd4382fce1df744bf650058521055268f906c
nhantran1245 <ssaiyan123410@gmail.com>
2020-03-25 15:08:25 +0700

    php

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+
+
+<div id="footer">
+    <div id="contact_logo">
+        <ul>
+            <li><a><img src="image/FB-logo.png" alt="FB logo"></a></li>
+            <li><a><img src="image/TW-logo.png" alt="Twitter logo"></a></li>
+            <li><a><img src="image/Ins-logo.jpg" alt="Instagram logo"></a></li>
+        </ul>    
+    </div>
+    
+</div>
---
+++ index.php
@@ -0,0 +1,32 @@
+﻿<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>StudyWeb</title>
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <div id="menu">
+        <a href="index.html"><img src="image/logo.png" alt="logo" /></a>
+        <ul id="main_menu">
+            <li> <a href="index.html">Trang chủ</a> </li>
+            <li> <a href="about.html">Giới thiệu</a></li>
+            <li> <a href="testExams">Thi thử</a></li>
+            <li> <a href="document">Tài liệu</a></li>
+            <li> <a href="news">Tin tức</a></li>
+            <li> <a href="contact">Liên hệ</a></li>
+        </ul>
+        
+    </div>
+
+    <div id="content">
+
+    </div>
+    
+    <?php include("footer.php");
+    ?>
+   
+
+
+
+</body>
